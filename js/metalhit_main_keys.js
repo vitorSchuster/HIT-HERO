58aaf74a9d1cdb8a2431a33eb2dc1b3651f85b8b
vitor <vitorluisschuster@gmail.com>
2024-05-13 23:10:24 -0300

    controles funcionando
    consegui fazer um arquivo js que detecta quando se pressiona as teclas e altera o css dependendo da tecla

--- js/metalhit_main_keys.js
+++ js/metalhit_main_keys.js
@@ -1,5 +1,48 @@
-document.addEventListener('keypress', function(event) {
-    if (event.key === 'Enter') {
-      console.log('alo')
-    }
-  });
+
+document.addEventListener('keydown', function (event) {
+  switch (event.key) {
+    case 'w':
+    case 'W':
+      document.getElementById('teclaW').style.backgroundColor = "blue"
+    break;
+    case 'a':
+    case 'A':
+      document.getElementById('teclaA').style.backgroundColor = "blue"
+
+      break;
+    case 's':
+    case 'S':
+      document.getElementById('teclaS').style.backgroundColor = "blue"
+      break;
+    case 'd':
+    case 'D':
+      document.getElementById('teclaD').style.backgroundColor = "blue"
+
+      break;
+  }
+
+});
+
+document.addEventListener('keyup', function (event) {
+  switch (event.key) {
+    case 'w':
+    case 'W':
+      document.getElementById('teclaW').style.backgroundColor = "var(--cor-tecla)"
+    break;
+    case 'a':
+    case 'A':
+      document.getElementById('teclaA').style.backgroundColor = "var(--cor-tecla)"
+
+      break;
+    case 's':
+    case 'S':
+      document.getElementById('teclaS').style.backgroundColor = "var(--cor-tecla)"
+      break;
+    case 'd':
+    case 'D':
+      document.getElementById('teclaD').style.backgroundColor = "var(--cor-tecla)"
+
+      break;
+  }
+
+});
