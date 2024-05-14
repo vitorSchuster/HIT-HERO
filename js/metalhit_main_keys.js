54dcaa518eda3da5807650ddd6963e6918636faf
vitor <vitorluisschuster@gmail.com>
2024-05-14 00:33:50 -0300

    criando obstacúlos para o player part 01

--- js/metalhit_main_keys.js
+++ js/metalhit_main_keys.js
@@ -1,10 +1,13 @@
+var posição_elementos = ["elemento0", "elemento1", "elemento2", "elemento3"]
+
 
 document.addEventListener('keydown', function (event) {
   switch (event.key) {
     case 'w':
     case 'W':
       document.getElementById('teclaW').style.backgroundColor = "blue"
-    break;
+
+      break;
     case 'a':
     case 'A':
       document.getElementById('teclaA').style.backgroundColor = "blue"
@@ -28,7 +31,7 @@ document.addEventListener('keyup', function (event) {
     case 'w':
     case 'W':
       document.getElementById('teclaW').style.backgroundColor = "var(--cor-tecla)"
-    break;
+      break;
     case 'a':
     case 'A':
       document.getElementById('teclaA').style.backgroundColor = "var(--cor-tecla)"
@@ -46,3 +49,28 @@ document.addEventListener('keyup', function (event) {
   }
 
 });
+
+//elementos "obstacúlos"
+
+// function randomElement(a,b){
+//   return Math.floor(Math.random () * (b - a + 1)) + a
+// }
+
+function randomElement(a, b) {
+  return Math.floor(Math.random() * (b - a + 1)) + a
+}
+
+switch (randomElement(1, 3)) {
+  case 0:
+    console.log(posição_elementos[0])
+    break
+  case 1:
+    console.log(posição_elementos[1])
+    break
+  case 2:
+    console.log(posição_elementos[2])
+    break
+  case 3:
+    console.log(posição_elementos[3])
+    break
+}
